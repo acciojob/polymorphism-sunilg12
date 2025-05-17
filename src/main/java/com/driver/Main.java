a5004ceec379f6efc3ac43cbbc062dbdf15fdacb
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-05-17 12:41:51 +0530

    Polymorphism assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,29 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println(p.product(2,4));
+        System.out.println(p.product(4,5,3));
+        System.out.println(p.product(4.56, 3.45));
+
+    }
+
+
 }
